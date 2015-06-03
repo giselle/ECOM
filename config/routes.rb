5c83586b25dcdf4ea6658188b8e0f5ff1adf6f22
Giselle Gonzalez <giselle@growthmodedigital.com>
2015-06-02 23:28:25 -0500

    Added new products to store

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   get "/products/new" => 'products#new'
 
+  post "/products" => 'products#create'
+
   get "/products/:id" => 'products#show'
-
-   post "/products" => 'products#create'
-
+  
   get "/products/:id/edit" => 'products#edit'
 
-  patch "/prodcuts/:id" => 'products#update'
+  patch "/products/:id" => 'products#update'
 
   delete "/products/:id" => 'products#destroy'
 end
